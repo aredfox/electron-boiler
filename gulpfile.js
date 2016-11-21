86985aa608f7061635ad663bf851a5d234cc096d
Yves Schelpe <yves.Schelpe@kdg.be>
2016-11-21 16:59:04 +0100

    added colors and themes for gulp informational debugging on console.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const del = require('del');
 const ignore = require('gulp-ignore');
 const runSequence = require('run-sequence');
 const argv = require('yargs').argv;
+const colors = require('colors/safe');
+/* ******************************************************************** */
 /* LOCATIONS */
 const SOURCE_DIR = path.resolve('./src');
 const SOURCE_DATA_DIR = path.resolve('./src/data');
@@ -16,18 +18,21 @@ const SOURCE_VIEWS_DIR = path.resolve('./src/views');
 const SOURCE_LIB_DIR = path.resolve('./src/lib');
 const SOURCE_STYLES_DIR = path.resolve('./src/styles');
 const BUILD_DIR = path.resolve('./app');
+/* ******************************************************************** */
 /* FILE IMPORTS */
 const packagejson = require('./package.json');
 /*/********************************************************************///
+/*///*/
 
 /* ******************************************************************** */
 /* DEBUG TASKS */
 const TASK_DEBUG = 'debug';
 gulp.task(TASK_DEBUG, cb => {
-    log(`Running in '${getEnvironmentName()}'-environment.`);
+    log(`Running GULP for project '${getProjectName()}' in '${getEnvironmentName()}'-environment.`);
     cb();
 });
 /*/********************************************************************///
+/*///*/
 
 /* ******************************************************************** */
 /* CLEAN TASKS */
@@ -42,7 +47,7 @@ gulp.task(TASK_CLEAN, cb => {
 const TASK_CLEAN_BUILD_DIR = `${TASK_CLEAN}:build`;
 gulp.task(TASK_CLEAN_BUILD_DIR, cb => {
     const dirglob = `${BUILD_DIR}/**/*`;
-    log(`Will now clean with glob '${dirglob}'.`);
+    logWarning(`Will now clean with glob '${dirglob}'.`);
     return del([
         `${dirglob}`,
         `!${dirglob}/*.gitkeep`
@@ -50,14 +55,15 @@ gulp.task(TASK_CLEAN_BUILD_DIR, cb => {
     cb);                    
 });
 /*/********************************************************************///
+/*///*/
 
 /* ******************************************************************** */
 /* HELPER ENVIRONMENT METHODS */
 function getEnvironmentName() {
     if(isProd()) {
-        return "Production";
+        return `Production`;
     }
-    return "Development";
+    return `Development`;
 }
 function isDev() {
     return !isProd();
@@ -65,11 +71,40 @@ function isDev() {
 function isProd() {
     return argv.prod;
 }
-/* HELPER CONSOLE LOG METHODS */
-function log(message) {
-    console.log(` --> [${getFormattedTime()}] ${message}`);
+/* ******************************************************************** */
+/* HELPER PACKAGE JSON METHODS */
+function getProjectName() {
+    if(packagejson === undefined) {
+        logError('No package.json defined');
+        throw new Error('No package.json defined');
+    }
+    if(packagejson['name'] === undefined) {
+        logError('No \'name\' property found inside package.json');
+        throw new Error('No \'name\' property found inside package.json');
+    }
+    return packagejson.name;
 }
+/* ******************************************************************** */
+/* HELPER CONSOLE LOG METHODS */
+const LOG_DEBUG = 'dbg';
+const LOG_INFO = 'inf';
+const LOG_WARNING = 'wng';
+const LOG_ERROR = 'err';
+colors.setTheme({
+    thdbg: ['bgBlack', 'grey'],
+    thinf: ['bgWhite', 'blue'],
+    thwng: ['bgYellow', 'black'],
+    therr: ['bgRed', 'white']
+});
+function log(message, type) {    
+    type = type || LOG_DEBUG;
+    console.log(colors['th'+type](` > ${[type.toUpperCase()]} [${getFormattedTime()}] ${message}`));
+}
+function logInfo(message) { log(message, LOG_INFO); }
+function logWarning(message) { log(message, LOG_WARNING); }
+function logError(message) { log(message, LOG_ERROR); }
 function getFormattedTime() {
     return moment().format('HH:mm:ss');
 }
 /*/********************************************************************///
+/*///*/
