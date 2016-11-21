f595fad41392836cdfd0a1f714ad6b03b343757d
Yves Schelpe <yves.schelpe@kdg.be>
2016-11-21 15:43:10 +0100

    added clean task for the app folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 const gulp = require('gulp');
 const path = require('path');
 const moment = require('moment');
+const del = require('del');
+const ignore = require('gulp-ignore');
+const runSequence = require('run-sequence');
+const argv = require('yargs').argv;
 /* LOCATIONS */
 const SOURCE_DIR = path.resolve('./src');
 const SOURCE_DATA_DIR = path.resolve('./src/data');
@@ -12,8 +16,6 @@ const SOURCE_VIEWS_DIR = path.resolve('./src/views');
 const SOURCE_LIB_DIR = path.resolve('./src/lib');
 const SOURCE_STYLES_DIR = path.resolve('./src/styles');
 const BUILD_DIR = path.resolve('./app');
-/* PATTERNS, GLOBS, ETC */
-const ignoreFiles = ['*.gitkeep'];
 /* FILE IMPORTS */
 const packagejson = require('./package.json');
 /*/********************************************************************///
@@ -21,14 +23,53 @@ const packagejson = require('./package.json');
 /* ******************************************************************** */
 /* DEBUG TASKS */
 const TASK_DEBUG = 'debug';
-gulp.task(TASK_DEBUG, () => {    
+gulp.task(TASK_DEBUG, cb => {
+    log(`Running in '${getEnvironmentName()}'-environment.`);
+    cb();
 });
 /*/********************************************************************///
 
 /* ******************************************************************** */
+/* CLEAN TASKS */
+const TASK_CLEAN = 'clean';
+gulp.task(TASK_CLEAN, cb => {
+    runSequence(
+        TASK_DEBUG,
+        TASK_CLEAN_BUILD_DIR,
+        cb
+    );    
+});
+const TASK_CLEAN_BUILD_DIR = `${TASK_CLEAN}:build`;
+gulp.task(TASK_CLEAN_BUILD_DIR, cb => {
+    const dirglob = [
+        `app/**/*`        
+    ];
+    log(`Will now clean with glob '${dirglob}'.`);
+    return del([
+        `${dirglob}`,
+        `!${dirglob}/*.gitkeep`
+    ], 
+    cb);                    
+});
+/*/********************************************************************///
+
+/* ******************************************************************** */
+/* HELPER ENVIRONMENT METHODS */
+function getEnvironmentName() {
+    if(isProd()) {
+        return "Production";
+    }
+    return "Development";
+}
+function isDev() {
+    return !isProd();
+}
+function isProd() {
+    return argv.prod;
+}
 /* HELPER CONSOLE LOG METHODS */
 function log(message) {
-    console.log(` ** [${getFormattedTime()}] ${message}`);
+    console.log(` --> [${getFormattedTime()}] ${message}`);
 }
 function getFormattedTime() {
     return moment().format('HH:mm:ss');
